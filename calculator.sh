05f728bb301100cdb290a35fe2746016f22a496e
SiddheshDare <100286243+SiddheshDare@users.noreply.github.com>
2025-02-26 11:42:36 +0530

    Update calculator.sh

--- calculator.sh
+++ calculator.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-# A simple script to run the Java ScientificCalculator program
-
 # Run the Java program
-java -cp target/ScientificCalculator.jar org.example.ScientificCalculator
+java -cp "target/*" org.example.ScientificCalculator
